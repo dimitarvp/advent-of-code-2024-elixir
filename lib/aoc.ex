03d5a6ab38e64335a2a68d2cba820157c3645f7e
Dimitar Panayotov <mitko.p@gmail.com>
2024-12-01 17:03:28 +0200

    remote initial boilerplate

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -2,17 +2,4 @@ defmodule Aoc do
   @moduledoc """
   Documentation for `Aoc`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
