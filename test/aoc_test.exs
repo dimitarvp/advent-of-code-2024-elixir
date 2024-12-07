fdf3acf1ea889c5d00021d8a45bdeaf68704f6c7
Dimitar Panayotov <mitko.p@gmail.com>
2024-12-07 15:09:00 +0200

    add tests for the actual puzzle inputs

--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -1,5 +1,13 @@
 defmodule AocTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Aoc
   doctest Day01
+
+  test "Day 1, part 1" do
+    assert Day01.part_1() == 2_580_760
+  end
+
+  test "Day 1, part 2" do
+    assert Day02.part_2() == 25_358_365
+  end
 end
